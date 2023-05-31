ef8499302b58d1e3e983dac253b2da24ed02a9ec
S-Gabrie1 <sergegabriel24@gmail.com>
2023-05-31 13:57:40 +0200

    initial

---
+++ package/index.js
@@ -0,0 +1,9 @@
+import lightMode from "package1s";
+
+function messageToggler() {
+    return (
+        lightMode ? "You are now in Light mode" : "You are now in Dark Mode"
+    );
+}
+
+export default messageToggler;
